5efc04068c6464aedddd42dadddb292db47aa3d0
shivam <shivampatle12345@gmail.com>
2025-07-22 22:37:05 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,4 @@
+// Vercel does not support custom Node.js servers.
+// To deploy on Vercel, remove this file and use Next.js default build and start commands.
+// All routing and API logic should be handled via Next.js pages and API routes.
+// See https://vercel.com/docs/concepts/deployments/overview for more info.
