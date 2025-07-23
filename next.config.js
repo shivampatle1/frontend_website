d1fc5aee789c7c30095895220408f8aac054bb8c
Shivam Patle <144086437+shivampatle1@users.noreply.github.com>
2025-07-23 09:24:01 +0000

    next changes

--- next.config.js
+++ next.config.js
@@ -1,30 +1,28 @@
 /** @type {import('next').NextConfig} */
 const runtimeCaching = require("next-pwa/cache");
-const withPWA = require('next-pwa');
+const withPWA = require("next-pwa");
+
 const { hostname } = new URL(`${process.env.NEXT_PUBLIC_BASE_URL}`);
-const nextConfig={
+
+const baseConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    domains: [`${hostname}`]
+  experimental: {
+    forceSwcTransforms: true, // ensure native SWC is used
   },
-}
+  images: {
+    domains: [`${hostname}`],
+  },
+};
+
 const pwa = process.env.NEXT_PWA_STATUS;
-const nextConfigWithPwa = withPWA({
+
+const pwaConfig = withPWA({
   dest: "public",
   register: true,
   skipWaiting: true,
   runtimeCaching,
   disable: process.env.NODE_ENV === "development",
-}) (
-    nextConfig
-);
-module.exports = pwa==='1'?nextConfigWithPwa:nextConfig ;
+});
 
-// next.config.js
-module.exports = {
-  swcMinify: true,
-  experimental: {
-    forceSwcTransforms: true, // use native SWC instead of buggy WASM
-  },
-};
+module.exports = pwa === "1" ? pwaConfig(baseConfig) : baseConfig;
